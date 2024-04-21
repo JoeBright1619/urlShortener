b4af466ca88241e2cb95415c29d620f9255322d2
JoeBright1619 <brightcollin1619@gmail.com>
2024-04-21 22:40:58 +0200

    feat: first work

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
@@ -14,11 +17,40 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+var urlArr = [];
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  
+  const url = req.body.url
+
+  if(/http:\/\/www\..+\.com/.test(url)){
+   // res.send({ URL: url});
+    console.log(`URL: ${url}`)
+
+  var hasObj = urlArr.find(item=>item.url==url);
+    if(hasObj){
+      res.send({
+        original_url: hasObj.url,
+        short_url: hasObj.id
+      })
+      console.log(hasObj.id)
+    }
+    else{
+      urlArr.push({
+        url: url,
+        id: urlArr.length + 1
+      })
+      console.log(urlArr)
+    }
+  }
+  else{
+    console.log("error: url notfound")
+  }
+  
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+  const random = Math.random();
+  console.log(urlArr)
 });
